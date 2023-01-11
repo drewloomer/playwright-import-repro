a67454e0d43c48f4bf7fc4f8769ac3887b46d163
Drew Loomer <drew.loomer@curology.com>
2023-01-11 10:51:32 -0500

    Remove old tests.

--- 1.26/example.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "@playwright/test";
-import { gimmeAOne } from "./playwright-utils.js";
-
-test("imports work", () => {
-  expect(gimmeAOne()).toBe(1);
-});
--- 1.27/playwright-utils.ts
+++
@@ -1,4 +0,0 @@
-/** This function returns 1. Incredibly useful. */
-export function gimmeAOne() {
-  return 1;
-}
