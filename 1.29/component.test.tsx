05ee6a2a979673ee7f82cff8709bcb640fd5f294
Drew Loomer <drew.loomer@curology.com>
2023-01-11 10:59:45 -0500

    Update Readme

--- 1.29/component.test.tsx
+++ 1.29/component.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { test } from '@playwright/experimental-ct-react';
 
 // Uncomment this and things blow up with "Cannot find module"
-// import { TestComponent } from "./Component.js";
+import { TestComponent } from "./Component.jsx";
 
-// test("imports work", ({ mount }) => {
-//   mount(<TestComponent />);
-// });
+test("imports work", ({ mount }) => {
+  mount(<TestComponent />);
+});
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# Repro for #18077
+# Repro for #20039
 
-Repro for https://github.com/microsoft/playwright/issues/18077.
+Repro for https://github.com/microsoft/playwright/issues/20039.
 
-- ✅ [./1.26/](./1.26/): works (run `npm i && npm test`)
-- ❌ [./1.27/](./1.27/): doesn’t work (run `npm i && npm test`)
+- ❌ [./1.29/](./1.29/): doesn’t work (run `npm i && npm test`)
